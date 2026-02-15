d6ff84cad6e0046ea8424e397983b3b298236b04
StephenRHarris <Sharris@gmail.com>
2026-02-14 20:44:16 -0600

    e: added player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 from logger import log_state
 def main():
     print(f"\nStarting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,14 +10,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pyClock = pygame.time.Clock()
     dt = 0
+    player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = pyClock.tick(60)/1000
+
         #print(f"time: {dt}")
 
 
